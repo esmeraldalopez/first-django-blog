02a0011d301a1fdeec92183f1ce1483628a786c9
Esme <esmeralda.lopez.e@gmail.com>
2018-03-21 21:04:38 -0600

    Plantilla de html modificada pra mostrar los posts que ya estan en la DB ... html/template tags

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post #incluyo el modelo definido en models, .models indica que buscara en el mismo directorio
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #QuerySet
+    return render(request, 'blog/post_list.html', {'posts':posts})

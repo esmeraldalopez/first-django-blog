1e3db35360f843e6423937b64561c91e37aaefa8
Esme <esmeralda.lopez.e@gmail.com>
2018-03-22 18:21:50 -0600

    CSS  y Vista, url y template de post_detail añadidas

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,12 @@ from . import views #importamos todas nuestras views
 #primer patron URL
 urlpatterns = [
     url(r'^$', views.post_list),  #asignamos una view llamada post_list al url '^$'
+           #/post/12345678904521/ el numero es = pk
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    #?P<pk>[0-9]+) Django llevara todo lo que coloque aqui y lo tranferirá a una vista como una variable llamada pk que es la abreviacion de Primary Key, que se utiliza a menudo en proyectos de Django pero podemos llamarlo como gustemos ej: post_id
+     #0-9 dice que solo puede ser un numero, NO una letra
+     #significa que debe uno o mas digitos, ES DECIR
+     #http://127.0.0.1:8000/post// no es correcto
+     #http://127.0.0.1:8000/post/1234567890/ SI ES CORRECTO
 ]
+#URL para el view post_detail http://127.0.0.1:8000/post/1/
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post #incluyo el modelo definido en models, .models indica que buscara en el mismo directorio
 
@@ -6,3 +6,9 @@ from .models import Post #incluyo el modelo definido en models, .models indica q
 def post_list(request):
     posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #QuerySet
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+#post_detail recibe pk, que es lo que recibira en la url, que bien puede ser el numero de blog, para este caso
+def post_detail(request, pk):
+    #QuerySet para buscar el post
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})

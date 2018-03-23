85d838dadff48fc7b9e0d4d1f6a7ada84fa32d62
Esme <esmeralda.lopez.e@gmail.com>
2018-03-23 16:22:42 -0600

    Ahora los post se ordenan del mas reciente al mas antiguo

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post #incluyo el modelo definido en models, .models indica q
 from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #QuerySet
+    posts =  Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #QuerySet
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 #post_detail recibe pk, que es lo que recibira en la url, que bien puede ser el numero de blog, para este caso
